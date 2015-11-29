f767962e75a15668e901f8f48eb27ad891a9ac4a
mlanden <mlanden@umbc.edu>
2015-11-29 10:23:57 -0500

    basic layout

--- InfoSweet/src/Dashboard.java
+++ InfoSweet/src/Dashboard.java
@@ -1,12 +1,41 @@
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 public class Dashboard extends JPanel {
+	private JLabel folderNaame;
+	private JButton browse, sort;
 	Dashboard(){
+		JPanel folder = new JPanel();
 		setPreferredSize(new Dimension(500, 500));
-		add(new JLabel());
+		//setLayout(new GridLayout(0,2));
+		folderNaame = new JLabel("Main storage folder");
+		folder.add(folderNaame);
+		browse = new JButton("Browse");
+		browse.addActionListener(new actions());
+		folder.add(browse);
+		
+		JPanel function = new JPanel();
+		sort = new JButton("Sort all files on computer");
+		function.add(sort);
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		add(folder);
+		add(function);
+	}
+	
+	private class actions implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == browse){
+				
+			}
+		}
+		
 	}
 }
